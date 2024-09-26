b26b8409a3a8762ee3f57cca67a1d109462e670f
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2024-09-26 23:05:19 +0700

    feat: added pagination with fetchMore Apollo for media list Anime

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,49 @@ import { useQuery } from '@apollo/client';
 import GetMediaPages from './queries/GetMediaPages.gql';
 import styles from './App.module.css';
 import AniCard from './components/Card/AniCard';
+import Pagination from './components/Paginations';
 
 function App() {
-  const { data, loading, error } = useQuery(GetMediaPages);
-  console.log({
-    data,
+  const { data, loading, error, fetchMore } = useQuery(GetMediaPages, {
+    notifyOnNetworkStatusChange: true, // agar loading saat fetching data
+    variables: {
+      search: 'boruto',
+    }
   });
+
+  const navigate = (newPage) => {
+    fetchMore({
+      variables: {
+        page: newPage,
+      },
+      // cache query update
+      updateQuery: (prevData, { fetchMoreResult }) => {
+        return {
+          ...prevData,
+          Page: {
+            ...prevData.Page,
+            pageInfo: fetchMoreResult.Page.pageInfo,
+            media: fetchMoreResult.Page.media,
+          },
+        };
+      },
+    });
+  };
+
+  // console.log({
+  //   data,
+  // });
 
   if (error) {
     console.log(error);
   }
 
+  const pageInfo = data?.Page.pageInfo || {};
+
   return (
-    <main>
+    <>
       <h1 className={styles.title}>Anime List</h1>
+      <input type="text" className={styles.search} placeholder="Search Anime" />
       <div className={styles.listWrapper}>
         {loading ? (
           <h2>Loading List...</h2>
@@ -31,7 +60,9 @@ function App() {
           })
         )}
       </div>
-    </main>
+
+      <Pagination hasNextPage={pageInfo.hasNextPage} onNext={() => navigate(pageInfo.currentPage + 1)} currentPage={data?.Page.pageInfo.currentPage} onPrevious={() => navigate(pageInfo.currentPage - 1)} />
+    </>
   );
 }
 
---
+++ src/components/Paginations/Pagination.jsx
@@ -0,0 +1,16 @@
+import styles from './Pagination.module.css';
+
+const Pagination = (props) => {
+  const { onPrevious, onNext, currentPage, hasNextPage } = props;
+  return (
+    <div className={styles.container}>
+      <button onClick={onPrevious} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <h4>{currentPage}</h4>
+      <button onClick={onNext} disabled={!hasNextPage}>Next</button>
+    </div>
+  );
+};
+
+export default Pagination;
